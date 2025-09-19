144e1551a33a22ce97870eed80f74310e2e420cd
Deiker <deiker@gmail.com>
2025-09-19 10:22:17 -0400

    Fix: CSRF PROTEC, HEADER

--- config/settings.py
+++ config/settings.py
@@ -10,3 +10,4 @@ JWT_TOKEN_LOCATION =os.getenv("JWT_TOKEN_LOCATION")   # Guardar en cookies
 JWT_COOKIE_SECURE = os.getenv("JWT_COOKIE_SECURE")  # True en producción con HTTPS
 JWT_COOKIE_HTTPONLY = os.getenv("JWT_COOKIE_HTTPONLY")
 JWT_COOKIE_SAMESITE =os.getenv("JWT_COOKIE_SAMESITE")  # O "Lax" si lo usas con frontend separad
+JWT_COOKIE_CSRF_PROTECT = os.getenv("JWT_COOKIE_CSRF_PROTECT")

dac313581cad26c40f93700f38d696e6988d1ff0
Daniel_Castillo <168136398+D4n13L-CSTL@users.noreply.github.com>
2025-09-22 22:49:15 -0400

    Update services.py

--- app/login/services.py
+++ app/login/services.py
@@ -28,24 +28,24 @@ class LoginAuth:
                 "access_token_cookie",
                 access_token,
                 httponly=True,
-                secure=False,   # True en producción con HTTPS
-                samesite="Lax",
+                secure=True,   # True en producción con HTTPS
+                samesite="None",
                 max_age=36000 
             )
             resp.set_cookie(
                 "tipo_de_user",
                 tipo_de_user,
                 httponly=True,
-                secure=False, 
-                samesite="Lax",
+                secure=True, 
+                samesite="None",
                 max_age=36000 
             )
             resp.set_cookie(
                 "id_usuario",
                 str(id_usuario),
                 httponly=True,
-                secure=False, 
-                samesite="Lax",
+                secure=True, 
+                samesite="None",
                 max_age=36000 
             )
 
@@ -55,8 +55,8 @@ class LoginAuth:
                     "id_club_cookie",
                     str(id_club),
                     httponly=True,
-                    secure=False,   # True en producción con HTTPS
-                    samesite="Lax",
+                    secure=True,   # True en producción con HTTPS
+                    samesite="None",
                     max_age=36000 
                 )  
             elif tipo_de_user == 'ATLETA':
@@ -64,16 +64,16 @@ class LoginAuth:
                     "id_club_atleta_cookie", #GUARDA EL ID DEL CLUB QUE PERTNECE EL ATLETA
                     str(id_club_atleta),
                     httponly=True,
-                    secure=False, 
-                    samesite="Lax",
+                    secure=True, 
+                    samesite="None",
                     max_age=36000 
                 )
                 resp.set_cookie(
                 "id_atleta",
                 str(id_atleta),
                 httponly=True,
-                secure=False,
-                samesite="Lax",
+                secure=True,
+                samesite="None",
                 max_age=36000 
                 )
             

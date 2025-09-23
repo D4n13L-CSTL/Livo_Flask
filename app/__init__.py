1d967f9d307eb25febe47cee1723bd391d2a3c84
Daniel_Castillo <168136398+D4n13L-CSTL@users.noreply.github.com>
2025-09-22 23:21:41 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -59,7 +59,7 @@ def create_app():
     api.init_app(app)
  
     
-    CORS(app, supports_credentials=True, origins=["http://localhost:5173", "http://localhost:3000", "http://172.0.0.1:5173" , "http://172.0.0.1:3000","http://127.0.0.1:5500","http://localhost:5500","https://bright-clocks-raise.loca.lt"])
+    CORS(app, supports_credentials=True, origins=["http://localhost:5173", "http://localhost:3000", "http://172.0.0.1:5173" , "http://172.0.0.1:3000","http://127.0.0.1:5500","http://localhost:5500","bright-clocks-raise.loca.lt"])
     
     api.add_namespace(api_auth)
     api.add_namespace(api_atleta)
